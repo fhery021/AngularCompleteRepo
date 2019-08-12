32ace9553f3413794df407dde84109224f6ad092
Ferenc Bondar <fhery021@gmail.com>
2019-08-12 23:49:40 +0300

    account service added, injectable

--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,23 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../services/logging.service';
+import { AccountsService } from '../services/accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  constructor(private loggingService: LoggingService) { }
-
-  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+    // this.accountsService.statusUpdated.subscribe(
+    //   (status: string) => alert('new status ' + status)
+    // );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
-    this.loggingService.logStatusChange(accountStatus);
+    this.accountsService.addAcount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
