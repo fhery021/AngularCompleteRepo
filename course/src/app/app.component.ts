da788223107c5dd5475caab58440696dd3bac38b
Ferenc Bondar <fhery021@gmail.com>
2019-11-28 21:06:28 +0200

    pipes added

--- course/src/app/app.component.ts
+++ course/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course';
+
+  appStatus = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'critical',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+
+  filteredStatus = '';
+
+  getStatusClasses(server: { instanceType: string, name: string, status: string, started: Date }) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(21, 11, 2019)
+    });
+  }
 }
